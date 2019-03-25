d6798ac5e36f4e8eff39366017096114171162a7
Shivam Yaduka <45012722+yshivam@users.noreply.github.com>
2019-03-25 15:57:55 +0530

    Update caas_docker.py

--- cgi-bin/caas_docker.py
+++ cgi-bin/caas_docker.py
@@ -20,7 +20,7 @@ usr = form.getvalue('usr')
 #if a[0] == 0:
 #container_name = sp.getstatusoutput("sudo ansible-playbook staas/shellinabox.yml --extra-vars='x={0}'".format(cname))
 
-#a1 = subprocess.getoutput("sudo docker build -t yshivam/flipper .")
+#a1 = subprocess.getoutput("sudo docker build -t shellinabox_test:v1 .")
 b = subprocess.getstatusoutput("sudo docker run -dit --name {0} shellinabox_test:v1".format(usr))
 if b[0] == 0:
 	print("<br><br>")
